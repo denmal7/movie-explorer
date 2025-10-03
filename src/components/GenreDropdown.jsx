2cbfd2d2e2415dd7cbfd10fb5f72d243cf35a2d0
Dennis Gitau <denniskimaru3@gmail.com>
2025-10-03 12:40:35 +0300

    made the dropdown menu responsive

--- src/components/GenreDropdown.jsx
+++ src/components/GenreDropdown.jsx
@@ -2,7 +2,7 @@
 
 const GenreDropdown = ({ genres, selectedGenre, onSelect }) => {
   return (
-    <div className="mb-4">
+    <div className="mb-4 mt-20">
         <select
           value={selectedGenre}
           onChange={(e) => onSelect(e.target.value)}
